15b0249740dde515a6f7572abe6f36422784c031
rodkwithnasa <34109135+rodkwithnasa@users.noreply.github.com>
2026-04-30 17:18:27 +0100

    coverage of app.js up to 86.48%

--- test/test.js
+++ test/test.js
@@ -10,6 +10,7 @@ const httpTerminator = createHttpTerminator({ server })
 
 //debugger;
 describe('POST /profile', function() {
+
   it('responds with json', function(done) {
 debugger;  
       request(app)
@@ -28,7 +29,7 @@ debugger;
     
   });
   describe('GET /temp?q=id', function () {
-      it('responds with temperature', function(done) {
+      it('responds with temperature on id 1 (inserted in post)', function(done) {
           request(app)
           .get('/temp?q=1')          
           .expect(200)
@@ -39,8 +40,44 @@ debugger;
             done()              
           })
       })
+	  it('responds with Not OK on out of range id', function(done) {
+		  request(app)
+		  .get('/temp?q=2')
+		  .expect(500)
+		  .end(function(err,res) {
+			  if (err) {done(err)}
+			  assert.equal(res.text, 'not ok')
+			  done()
+		  });
+	  });
   });
-  after('close server', function (done) {
+  describe('GET /', function () {
+	it('responds with Hello World',function(done) {
+		request(app)
+		.get('/')
+		.expect(200,/^Hello World!/)
+		.expect('Content-Type','text/html; charset=utf-8')
+		.end(function(err,res) {
+			if (err) {done(err)};
+			done();
+		});
+	});
+  });
+  describe('GET /sensor/:sensid/temp/:tempVal/door/:doorState', function() {
+	  it('responds with text and insert id',function(done) {
+		  request(app)
+		  .get(`/sensor/${sensorinstance.sensor}/temp/${sensorinstance.tempval}/door/${sensorinstance.doorstate}`)
+		  .expect(200,/^Id: \d+ Sensor: 12345 Temp: 18 Door: open$/)
+		  .expect('Content-Type','text/html; charset=utf-8')
+		  .expect('Content-Length', '39')
+		  .end(function(err,res) {
+			  if (err) {done(err)};
+			  done();
+		  });
+	  });
+  });
+		  
+	after('close server', function (done) {
     httpTerminator.terminate()
 	.then((res)=>{
 		console.log(`Terminate(then):${res}`);
